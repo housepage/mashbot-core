08442eefe55cd762abd64b0920b26e84492c2f61
Nick D'Andrea <nick@gnidan.org>
2010-04-20 19:10:37 -0400

    Filled in primitive entity

--- src/main/java/org/mashbot/server/types/web/PrimitiveEntity.java
+++ src/main/java/org/mashbot/server/types/web/PrimitiveEntity.java
@@ -10,24 +10,68 @@ public class PrimitiveEntity extends Entity implements MutableEntity {
   protected ArrayList<Assoc> assocs;
 
   public Assoc[] getAssoc() { return assocs.toArray(); }
+
   public boolean hasAssoc(Assoc assoc) { return assocs.contains(assoc); }
-  public void addAssoc(Assoc assoc);
-  public void removeAssoc(Assoc assoc);
+
+  public void addAssoc(Assoc assoc) { assocs.add(assoc); }
+
+  public void removeAssoc(Assoc assoc) { 
+    int index = assocs.indexOf(assoc);
+
+    if( index != -1 )
+      assocs.remove(index);
+  }
 
   /* Properties */
   protected ArrayList<Entity> properties;
 
-  public Entity[] getProperties();
-  public Entity getProperty(Ident id);
-  public Ident addProperty(Entity property);
-  public void removeProperty(Ident id);
+  protected int findIndex(Ident id)
+  {
+    int i;
+
+    for(i = 0; i < properties.size(); i++)
+    {
+      if(properties.get(i).getIdent().equals(id))
+      {
+        return i
+      }
+    }
+
+    return -1;
+
+  }
+
+  public Entity[] getProperties() { return properties.toArray(); }
+
+  public Entity getProperty(Ident id)
+  {
+    int index = findIndex(id);
+
+    if(index != -1)
+      return properties.get(i);
+    else
+      return null;
+  }
+
+  public Ident addProperty(Entity property)
+  {
+    properties.add(property);
+  }
+
+  public void removeProperty(Ident id)
+  {
+    int index = findIndex(id);
+
+    if(index != -1)
+      properties.remove(index);
+  }
 
   /* Primitive */
   protected PValue primitive = null;
 
   public boolean hasPrimitiveValue() { return primitive != null }
-  public PValue getPrimitiveValue();
-  public void setPrimitiveValue(PValue value);
-  public void removePrimitiveValue();
+  public PValue getPrimitiveValue() { return primitive; }
+  public void setPrimitiveValue(PValue value) { primitive = value; }
+  public void removePrimitiveValue() { primitve = null; }
 
 }
