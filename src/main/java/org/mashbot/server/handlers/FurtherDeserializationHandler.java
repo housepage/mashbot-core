beb7204b1cb57c65235f1f787d096d8af68e3af5
Andrew E. Gall <andrew@mashbot.net>
2010-05-05 11:18:18 -0700

    Adding further deserialization

---
+++ src/main/java/org/mashbot/server/handlers/FurtherDeserializationHandler.java
@@ -0,0 +1,32 @@
+package org.mashbot.server.handlers;
+
+import org.mashbot.server.exceptions.MashbotException;
+import org.mashbot.server.types.MObject;
+import org.mashbot.server.types.Request;
+import org.mashbot.server.types.RequestContext;
+import org.mashbot.server.types.Response;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class FurtherDeserializationHandler extends ChainableHandler {
+
+	Log log = LogFactory.getLog(getClass());
+	
+	@Override
+	public void postRequest(Request in, Response out, RequestContext context)
+			throws MashbotException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void preRequest(Request in, Response out, RequestContext context)
+			throws MashbotException {
+		MObject m = in.getMObject();
+		for(String field : m.getFields()){
+			log.warn("Field: "+field);
+			log.warn("Type:"+m.getField(field));
+		}
+	}
+
+}
